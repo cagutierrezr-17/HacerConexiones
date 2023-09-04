79c6a24a931948b14e2d51fdba10728e6350ecf4
Carlos Gutierrez <cagutierrezr.17@icloud.com>
2023-09-04 17:24:51 -0300

    Hacer Conexiones
    Tarea Practica - Hacer Conexiones

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("page loaded...");
+
+function changeName() {
+    var element = document.getElementById("name");
+    console.log(element);
+    element.innerHTML = "Carlos G";
+}
+
+function clean() {
+    var element = document.getElementById("name1");
+    element.innerHTML = ""
+}
+
+function clean1() {
+    var element1 = document.getElementById("name2");
+    element1.innerHTML = ""
+
+}
+
